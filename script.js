217115175a26c79fd43432ce6f46eac21ea5452a
IRIStecnologies <iristecnologies@protonmail.com>
2026-02-12 15:00:18 -0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-document.querySelectorAll(".hex").forEach(btn=>{
-    btn.addEventListener("click",()=>{
-        const id = btn.dataset.target;
-        const section = document.getElementById(id);
+document.querySelectorAll(".hex[data-target]").forEach(btn=>{
+btn.addEventListener("click",()=>{
+const id=btn.dataset.target;
+const section=document.getElementById(id);
 
-        if(section){
-            section.scrollIntoView({
-                behavior:"smooth"
-            });
-        }
-    });
+if(section){
+section.scrollIntoView({behavior:"smooth"});
+}
+});
 });
