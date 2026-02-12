88a460fca80a07593ef609686c5337cc7fbbbd0a
IRIStecnologies <iristecnologies@protonmail.com>
2026-02-12 15:50:37 -0300

    .

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 document.querySelectorAll(".hex[data-target]").forEach(btn=>{
 btn.addEventListener("click",()=>{
-const id=btn.dataset.target;
-const section=document.getElementById(id);
-
-if(section){
-section.scrollIntoView({behavior:"smooth"});
-}
+document.getElementById(btn.dataset.target)
+.scrollIntoView({behavior:"smooth"});
 });
 });
